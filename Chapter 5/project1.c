61d9cb876eb27e6dfee8e4da0952afdaa1a482f4
Aaron-Fuller2 <aaron.fuller@protonmail.com>
2026-07-13 16:02:21 -0400

    Calculates how many digits in a number up to 4 digits long

--- Chapter 5/project1.c
+++ Chapter 5/project1.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
 int main (void) {
-    printf("Enter a number up to 4 digits");
+    int digits, n;
+    printf("Enter a number up to 4 digits: ");
+    scanf("%d", &n);
+
+    if (n > 0 && n < 9) {
+        digits = 1;
+    } else if (n > 9 && n < 100) {
+        digits = 2;
+    } else if (n > 99 && n < 1000) {
+        digits = 3;
+    } else {
+        digits = 4;
+    }
+
+    printf("The number %d has %d digits\n", n, digits);
 }
