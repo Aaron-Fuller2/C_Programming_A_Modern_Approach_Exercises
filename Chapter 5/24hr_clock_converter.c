8ae7e6c3b7a01ee1dfefa97119504e2e2b21fa69
Aaron-Fuller2 <aaron.fuller@protonmail.com>
2026-07-13 16:28:36 -0400

    updated to hand the 12 PM case

--- Chapter 5/24hr_clock_converter.c
+++ Chapter 5/24hr_clock_converter.c
@@ -5,12 +5,14 @@ int main (void) {
     int minute;
 
     printf("Enter a 24-hour time: ");
-    scanf("%d:%d", &hour, &minute);
+    scanf("%2d:%2d", &hour, &minute);
 
     if (hour > 12) {
         hour -= 12;
-        printf("Equivalent 12-hour time: %d:%d PM\n", hour, minute);
+        printf("Equivalent 12-hour time: %02d:%02d PM\n", hour, minute);
+    } else if (hour == 12) {
+        printf("Equivalent 12-hour time: %02d:%02d PM\n", hour, minute);
     } else {
-        printf("Equivalent 12-hour time: %d:%d AM\n", hour, minute);
+        printf("Equivalent 12-hour time: %02d:%02d AM\n", hour, minute);
     }
 }
