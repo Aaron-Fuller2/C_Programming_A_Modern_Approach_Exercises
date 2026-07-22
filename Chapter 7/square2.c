7a225f5e1377de1dac4a6a1a1b04866e297e592e
Aaron-Fuller2 <aaron.fuller@protonmail.com>
2026-07-22 09:52:17 -0400

    exercise 1 and 2 are done

---
+++ Chapter 7/square2.c
@@ -0,0 +1,24 @@
+/* Prints a table of squares using for loop */
+
+#include <stdio.h>
+
+int main(void) {
+    int i, n, pause, ch;
+
+    printf("This program prints a table of squares.\n");
+    printf("Enter number of entries in table: ");
+    scanf("%d", &n);
+    getchar();
+
+    for (i= 1, pause = 0; i <= n; i++) {
+        printf("%10d%10d\n", i, i * i);
+        pause++;
+        if (pause == 24) {
+            printf("Press Enter to continue...");
+            getchar();
+            pause = 0;
+        }
+    }
+
+    return 0;
+}
